3e68b9b0b91e1f976269b1dacb9882d6e0c154e9
chooie <chebert@spotx.tv>
2018-05-01 17:31:46 -0600

    Test the message generated by the assertion

---
+++ main_test.go
@@ -0,0 +1,31 @@
+package assert_test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/chooie/assert"
+)
+
+func TestEqual(t *testing.T) {
+	assert.Equal(t, "yo", "yo", "Supports strings")
+	assert.Equal(t, 1, 1, "Supports numbers")
+}
+func TestMakeEqualTestMessageWithNoExtraMessages(t *testing.T) {
+	actual := assert.MakeEqualTestMessage(1, 2, []string{})
+	expected := "\n\nExpected <int>2,\nBut got <int>1\n"
+	errorIfUnequalStrings(t, actual, expected)
+}
+
+func TestMakeEqualTestMessageWithExtraMessages(t *testing.T) {
+	actual := assert.MakeEqualTestMessage("hi", "bye", []string{"Some message"})
+	expected := "\nSome message\n\nExpected <string>bye,\nBut got <string>hi\n"
+	errorIfUnequalStrings(t, actual, expected)
+}
+
+func errorIfUnequalStrings(t *testing.T, string1, string2 string) {
+	if strings.Trim(string1, "") != strings.Trim(string2, "") {
+		t.Error("\nExpected:\n" + string1)
+		t.Error("\nTo equal:\n" + string2)
+	}
+}
